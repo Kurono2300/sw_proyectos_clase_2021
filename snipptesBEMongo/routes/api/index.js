d37acd1d2d024bd650baa67b42cb341a8d0d4254
Emilio Escoto <emilioescoto23@gmail.com>
2021-06-30 22:39:38 -0600

    JWT, Login y Sign In

--- snipptesBEMongo/routes/api/index.js
+++ snipptesBEMongo/routes/api/index.js
@@ -1,12 +1,44 @@
 var express = require("express");
 var router = express.Router();
+const passport = require('passport');
 
+const passportJWT = require('passport-jwt');
+const extractJWT = passportJWT.ExtractJwt;
+const strategyJWT = passportJWT.Strategy;
+
+passport.use(
+    new strategyJWT(
+        {
+            jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey:process.env.JWT_SECRET
+        },
+        (payload, next) =>{
+            console.log(payload);
+            return next(null,payload);
+        }
+    )
+)
+
+const jwtMiddleWare = passport.authenticate('jwt',{session:false});
+
+const security_routes = require("./security/index");
 var snippets_routes = require("./snippets/index");
+
+router.use("/security", security_routes);
+router.use("/snippets", jwtMiddleWare, snippets_routes);
+//router.use("/snippets", snippets_routes);
+
+
+
+
 var camisas_routes = require("./camisas/index");
 var helpdesk_routes = require("./helpdesk/index");
-
-router.use("/snippets", snippets_routes);
 router.use("/camisas", camisas_routes);
 router.use("/helpdesk", helpdesk_routes);
+
+
+
+
+
 
 module.exports = router;
